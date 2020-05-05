2df8dead1403a4428976f76f31278c01e1424536
leofofeo <leo.a.rubiano@gmail.com>
2020-05-04 20:47:02 -0500

    basic plumbing of ethereum contract

---
+++ src/web3.js
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+
+if (window.ethereum) {
+	window.web3 = new Web3(window.ethereum);
+	window.ethereum.enable();
+}
+const web3 = new Web3(window.ethereum);
+
+export default web3;
